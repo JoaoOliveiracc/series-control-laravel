9dcac24202731d69625ef367f56b5b7d5ac196c2
JoaoOliveiracc <ccjoao_monteiro@outlook.com>
2024-11-27 10:55:01 -0300

    showing seasons

--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -8,8 +8,8 @@
   @endisset
   <ul class="list-group">
     @foreach ($series as $serie)
-      <li class="list-group-item d-flex justify-content-between align-items-center">{{ $serie->name }}
-
+      <li class="list-group-item d-flex justify-content-between align-items-center">
+        <a href="{{route('seasons.index', $serie->id)}}">{{ $serie->name }}</a>
         <span class="d-flex ">
           <a href="{{route('series.edit', $serie->id)}}" class="btn btn-primary btn-sm">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
