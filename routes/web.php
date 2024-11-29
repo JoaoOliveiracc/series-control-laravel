1cfb1293db2f37e0a37ca677538010ca7179efab
JoaoOliveiracc <ccjoao_monteiro@outlook.com>
2024-11-29 19:01:24 -0300

    listing episodes

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function() {
   return redirect('/series');
 });
-
 Route::resource('/series', SeriesController::class)
   ->except(['show']);
-  
 Route::get('/series/{series}/seasons', [SeasonsController::class, 'index'])->name('seasons.index');
+
+Route::get('/seasons/{season}/episodes', [\App\Http\Controllers\EpisodesController::class, 'index'])->name('episodes.index');
+Route::post('/seasons/{season}/episodes', [\App\Http\Controllers\EpisodesController::class, 'index'])->name('episodes.index');
